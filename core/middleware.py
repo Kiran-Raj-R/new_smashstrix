e5c8f06c38b0a3fedf79823c6c928cf08c626f36
Kiran-Raj-R <kiranrajendran130799@gmail.com>
2026-01-17 02:42:12 +0530

    new AccessControlMiddleware for role-seperation, user-blocking and caching

--- core/middleware.py
+++ core/middleware.py
@@ -1,24 +1,29 @@
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib import messages
+from django.utils.cache import add_never_cache_headers
 
-class RoleAccessMiddleware:
+class AccessControlMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated:
-            if request.path.startswith("/user") or request.path in ["/","/home/","/shop/"]:
-                if request.user.is_staff:
-                    logout(request)
-                    request.session.flush()
-                    messages.error(request, "Admin accounts cannot access user pages.")
-                    return redirect("login")
-        
-            if request.path.startswith("/adminpanel"):
-                if not request.user.is_staff:
-                    logout(request)
-                    request.session.flush()
-                    messages.error(request,"Unauthorised access.")
-                    return redirect("login")
-        return self.get_response(request)
+        user = request.user
+        path = request.path
+        response = self.get_response(request)
+
+        if user.is_authenticated:
+            add_never_cache_headers(response)
+            if hasattr(user, "blocked") and user.blocked:
+                logout(request)
+                messages.error(request, "Your account has been blocked.")
+                return redirect("login")
+
+            if path.startswith("/adminpanel") and not user.is_staff:
+                messages.error(request, "Unauthorized admin access.")
+                return redirect("login")
+
+            if not path.startswith("/adminpanel") and user.is_staff:
+                pass
+
+        return response
