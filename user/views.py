90d747af7648d4bf01264087d7280907153cdbd7
Kiran-Raj-R <kiranrajendran130799@gmail.com>
2026-01-28 00:38:28 +0530

    feat: Change password view added.

--- user/views.py
+++ user/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.core.paginator import Paginator
 from products.models import Product, Brand, Category, ColorVariant
 from .color_map import COLOR_MAP
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .models import Address
-from .forms import AddressForm, EditProfileForm
+from .forms import AddressForm, EditProfileForm, ChangePasswordForm
 from django.contrib import messages
 from accounts.utils import send_email_change_otp
 
@@ -13,20 +14,16 @@ def home(request):
     return render(request, "user/home.html")
 
 def shop(request):
-
     products = Product.objects.filter(active=True)
     q = request.GET.get("q")
     if q:
         products = products.filter(name__icontains=q)
-
     selected_categories = request.GET.getlist("category")
     if selected_categories:
         products = products.filter(category_id__in=selected_categories)
-
     selected_brands = request.GET.getlist("brand")
     if selected_brands:
         products = products.filter(brand_id__in=selected_brands)
-
     selected_colors = request.GET.getlist("color")
     if selected_colors:
         products = products.filter(colors__color__in=selected_colors).distinct()
@@ -37,10 +34,8 @@ def shop(request):
     if min_price == "0" and max_price == "0":
         min_price = None
         max_price = None
-
     if min_price:
         products = products.filter(price__gte=min_price)
-
     if max_price:
         products = products.filter(price__lte=max_price)
 
@@ -85,21 +80,14 @@ def shop(request):
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id, active=True)
-
     related_products = Product.objects.filter(category=product.category, active=True).exclude(id=product.id)[:4]
-
-    return render(request,"user/product_detail.html",{
-            "product": product,
-            "related_products": related_products,
-        },
-    )
+    return render(request,"user/product_detail.html",{"product": product,"related_products": related_products,},)
 
 @login_required(login_url="login")
 def user_profile(request):
     default_address = (
         Address.objects.filter(user=request.user, is_default=True).first())
     return render(request, "user/profile/profile.html", {"default_address": default_address})
-
 
 @login_required
 def address_list(request):
@@ -113,7 +101,6 @@ def address_add(request):
     if request.method == 'POST' and form.is_valid():
         address = form.save(commit=False)
         address.user = request.user
-
         if not Address.objects.filter(user=request.user).exists():
             address.is_default = True
         elif address.is_default:
@@ -159,9 +146,7 @@ def address_set_default(request, pk):
 def edit_profile(request):
     user = request.user
     old_email = user.email
-
     form = EditProfileForm(request.POST or None, instance=user)
-
     if request.method == "POST" and form.is_valid():
         new_email = form.cleaned_data["email"]
         user.first_name = form.cleaned_data["first_name"]
@@ -172,17 +157,14 @@ def edit_profile(request):
             send_email_change_otp(user, new_email)
             messages.info(request, "OTP sent to your new email for verification.")
             return redirect("verify_email_change")
-
         user.save()
         messages.success(request, "Profile updated successfully.")
         return redirect("profile")
-
     return render(request, "user/profile/edit_profile.html", {"form": form})
 
 @login_required
 def verify_email_change(request):
     user = request.user
-
     if request.method == "POST":
         otp = request.POST.get("otp")
         if user.otp == otp and not user.otp_expired():
@@ -192,10 +174,27 @@ def verify_email_change(request):
             user.otp_created = None
             user.otp_verified = True
             user.save()
-
             messages.success(request, "Email updated successfully.")
             return redirect("profile")
-
         messages.error(request, "Invalid or expired OTP.")
-
     return render(request, "user/profile/verify_email.html")
+
+@login_required
+def change_password(request):
+    form = ChangePasswordForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        user = request.user
+        old_password = form.cleaned_data["old_password"]
+        new_password = form.cleaned_data["new_password"]
+
+        if not user.check_password(old_password):
+            messages.error(request, "Current password is incorrect.")
+            return redirect("change_password")
+        user.set_password(new_password)
+        user.save()
+        logout(request)
+
+        messages.success(request, "Password changed successfully. Please login again.")
+        return redirect("login")
+    return render(request, "user/profile/change_password.html", {"form": form})
