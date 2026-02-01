2080c4cae1a45c75bd63e304e0976429b4c01c0f
Kiran-Raj-R <kiranrajendran130799@gmail.com>
2026-02-02 04:58:19 +0530

    feat: Reuse of otp verify template in edit profile email change

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth import login,logout,get_user_model
-from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from . models import User
 from . utils import send_otp, send_reset_password_otp
