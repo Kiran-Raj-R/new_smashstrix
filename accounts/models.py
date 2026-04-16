724fcaed402f668561d7a51319385dc0f5ac8936
Kiran-Raj-R <kiranrajendran130799@gmail.com>
2026-04-16 14:22:26 +0530

    refactor: referral code and referred by field added to User

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
+import uuid
 
 class User(AbstractUser):
     username = models.CharField(max_length=150,unique=False,blank=True,null=True)
@@ -16,9 +17,16 @@ class User(AbstractUser):
     otp_verified = models.BooleanField(default=False)
     mobile = models.CharField(max_length=10,unique=True,blank=True,null=True)
     profile_image = models.ImageField(upload_to="profiles/", null=True, blank=True)
+    referral_code = models.CharField(max_length=10,unique=True)
+    referred_by = models.ForeignKey("self",on_delete=models.SET_NULL,null=True,blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        if not self.referral_code:
+            self.referral_code = str(uuid.uuid4())[:8].upper()
+        super().save(*args, **kwargs)   
 
     def otp_expired(self):
         if not self.otp_created:
