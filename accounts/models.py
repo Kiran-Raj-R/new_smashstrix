713a0c3bcea1a8aa207049b212f340f9a967a630
Kiran-Raj-R <kiranrajendran130799@gmail.com>
2026-04-29 15:44:28 +0530

    fix: migration issue of db to postgres fixed.

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,32 @@ from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 import uuid
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if not extra_fields.get("is_staff"):
+            raise ValueError("Superuser must have is_staff=True")
+        if not extra_fields.get("is_superuser"):
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150,unique=False,blank=True,null=True)
+    username = None
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
@@ -22,6 +45,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def save(self, *args, **kwargs):
         if not self.referral_code:
